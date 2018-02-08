add14629103e4a82845ad30cb99d9e7f90eec585
Magdy Hasan <magdy.hasan17@hotmail.com>
2018-02-08 12:22:45 +0200

    Updated smartcab project.

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -97,7 +97,7 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-        if state not in self.Q.keys():
+        if self.learning and state not in self.Q.keys():
             self.Q[state] = {None: 0., 'forward': 0., 'left': 0., 'right': 0.}
         return
 
